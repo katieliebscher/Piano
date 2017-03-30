53f68e5498c76be2bfc7f2c7e6a999fc4277700d
katieliebscher <katieliebscher@gmail.com>
2017-03-30 12:59:05 -0400

    Add files via upload

--- Token.m
+++ Token.m
@@ -1,17 +1,17 @@
-function Token(handles)
+function Token(handles, hObject)
 
 
 
-color=get(handles.R6C5,'BackgroundColor');
+color=get(hObject,'BackgroundColor');
 disp(color);
 
 if color~=[1 0 0]
     if color~=[0 0 1]
-        set(handles.R6C5,'BackgroundColor','red');
+        set(hObject,'BackgroundColor','red');
     end
     
 else
-    set(handles.R6C5,'BackgroundColor',[0.9020 0.7804 0.2510])
+    set(hObject,'BackgroundColor',[0.9020 0.7804 0.2510])
 end
 
 
