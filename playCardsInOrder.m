6ac1465d81608ac8033e41f843b97ff31d2d2819
loyallikeadog <prticak97@gmail.com>
2017-04-09 18:02:01 -0400

    Add files via upload

---
+++ playCardsInOrder.m
@@ -0,0 +1,18 @@
+function playCardsInOrder(handles);
+
+load('sampleDeck.m');
+iCnt = 1; %This indicates the first card to the person
+% while iCnt <= 5;
+% for iCol = 1:2
+%     for iRow = 1:2
+%         tagName = ['R',playDeck(iCnt).LR,'C',playDeck(iCnt).LC];
+%         if strcmpi(playDeck(iCnt).number,handles.tagName.String)
+%             handles.tagName.Value = 1;
+%             pause(1);
+%             handles.tagName.Value = 0; 
+%         end
+%     end
+% end
+% iCnt = iCnt + 1;
+% end
+% end
