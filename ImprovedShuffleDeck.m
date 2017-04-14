1be66c53a08b35865a05918647385cefb4e6ec80
GabrielHBrown <gabriel.h.brown@gmail.com>
2017-04-14 18:52:47 -0400

    Add files via upload

---
+++ ImprovedShuffleDeck.m
@@ -0,0 +1,20 @@
+function ImprovedShuffleDeck
+v=linspace(1, 104, 104);
+% This function will shuffle every position of a vector or cell array and
+% move it to a random spot, switch the entry with a random entry.
+
+totalDeck = length(v);
+for iCnt = 1:totalDeck
+    switchSpot = randi(totalDeck);
+    x = v(iCnt);
+    y = v(switchSpot);
+    v(iCnt) = y;
+    v(switchSpot) = x;
+end
+for iCount=1:totaldeck
+    if v(iCount)>52
+        v(iCount)=v(iCount)-52;
+    end
+end
+shuffledDeck = v;
+
---
+++ randomChoice.m
@@ -0,0 +1,7 @@
+%randomChoice.m
+
+choice=card(randi(length(hand))).place;
+
+%choice will now have a string value
+
+%need to compare this to the data structure to see where 
